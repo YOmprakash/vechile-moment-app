00988b48f94f539403c5d416f75d4de5ca606a0f
YOmprakash <omprakashyamavaram@gmail.com>
2024-08-03 14:41:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import VehicleMap from './components/VehicleMap';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>Vehicle Movement on a Map</h1>
+      
+      <VehicleMap />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/VehicleMap.jsx
@@ -0,0 +1,20 @@
+
+const Map = () => {
+  
+
+  return (
+    <div style={{ width: "100%", height: "500px", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28940.556932670013!2d78.20798807227662!3d14.420952166866833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb3ebb7c5835cd9%3A0x3a4854affffebccd!2sPulivendula%2C%20Andhra%20Pradesh!5e1!3m2!1sen!2sin!4v1722675905950!5m2!1sen!2sin"
+        width="600"
+        height="450"
+       
+        allowfullscreen 
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Map;
